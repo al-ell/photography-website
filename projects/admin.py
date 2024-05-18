46ce7fdf544c4eb2920e018faae52933c13c79ad
Alice Dean <elliott.alice.92@gmail.com>
2024-05-18 13:22:35 +0000

    update admin

--- projects/admin.py
+++ projects/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Project, Photo
 
 
-admin.site.register(Project)
-admin.site.register(Photo)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'date',
+    )
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = (
+        'project',
+        'name',
+        'friendly_name',
+        'description',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Photo, PhotoAdmin)
--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+
 class Project(models.Model):
-    name = models.CharField(max_length=300)
-    friendly_name = models.CharField(max_length=300, null=False, blank=False)
+    # Prevent plural use in Django admin
+    class Meta:
+        verbose_name_plural = 'Project'
+
+    name = models.CharField(max_length=300, null=False, blank=False)
     description = models.TextField()
+    date = models.DateField(editable=True, null=False, blank=False)
 
     def __str__(self):
         return self.name
@@ -15,8 +20,10 @@ class Project(models.Model):
 class Photo(models.Model):
     project = models.ForeignKey('Project', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.name
