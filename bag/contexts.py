d161f58490b9def87c314421a4883258fa3ea3e0
Alice Dean <elliott.alice.92@gmail.com>
2024-05-21 11:27:33 +0000

    add bag context

---
+++ bag/contexts.py
@@ -0,0 +1,22 @@
+from decimal import Decimal
+from django.conf import settings
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = Decimal(settings.DELIVERY_FEE)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else: 
+        delivery = 0 
+        free_delivery_delta = 0
+    
+    grand_total = delivery + total
+
+    context = {bag_items, total, grand_total, free_delivery_delta, 
+                free_delivery_threshold, delivery, product_count}
+
+    return context
