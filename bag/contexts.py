8a086b8913ddc0b082ba16727c580cf75ee02074
Alice Dean <elliott.alice.92@gmail.com>
2024-05-22 17:11:00 +0000

    add size and price to print_info

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from shop.models import Prints
+from shop.models import Prints, PriceAndSize
 
 def bag_contents(request):
 
@@ -10,16 +10,28 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for prints_id, quantity in bag.items():
-        selected_print = get_object_or_404(Prints, pk=prints_id)
-        # total += quantity * selected_print.price
-        product_count += quantity
-        bag_items.append({
-            'prints_id': prints_id,
-            'quantity': quantity,
-            'selected_print': selected_print,
-        })
-
+    for prints_id, print_data in bag.items():
+        if isinstance(print_data, int):
+            selected_print = get_object_or_404(Prints, pk=prints_id)
+            # selected_price = 
+            # total += print_data * selected_price.price
+            product_count += print_data
+            bag_items.append({
+                'prints_id': prints_id,
+                'quantity': print_data,
+                'selected_print': selected_print,
+            })
+        else:
+            selected_print = get_object_or_404(Prints, pk=prints_id)
+            for size, quantity in print_data['items_by_size'].items():
+                # total += quantity * price
+                product_count += quantity
+                bag_items.append({
+                'prints_id': prints_id,
+                'quantity': print_data,
+                'selected_print': selected_print,
+                'size': size,
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = Decimal(settings.DELIVERY_FEE)
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from shop.models import PriceAndSize
 from shop.views import *
 
 
@@ -13,13 +13,24 @@ def add_to_bag(request, prints_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'price_radio' in request.POST:
+        size = request.POST['price_radio']
+
     bag = request.session.get('bag', {})
 
-    if prints_id in list(bag.keys()):
-        bag[prints_id] += quantity
+    if size:
+        if prints_id in list(bag.keys()):
+            if size in bag[prints_id]['items_by_size'].keys():
+                bag[prints_id]['items_by_size'][size] += quantity
+            else:
+                bag[prints_id]['items_by_size'][size] = quantity
+        else:
+            bag[prints_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[prints_id] = quantity
-    
+        messages.error(f'Please select a size to add to bag')
+
+
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
