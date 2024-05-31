72669f5c20ed6bbd917852c0d804bbeef110c4d4
Alice Dean <elliott.alice.92@gmail.com>
2024-05-31 20:07:23 +0000

    add order forms

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('date', 'order_number',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ( 'date', 'order_number', 'delivery_cost',
+              'order_total', 'grand_total', 'full_name',
+              'email', 'phone_number', 'street_address1',
+              'street_address2', 'town_or_city', 'county',
+              'postcode', 'country')
+
+    list_display = ( 'date', 'order_number', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'county', 'postcode', 'country',)
+
+    def __init__(self, *args, **kwargs):
+        
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'town_or_city': 'Town or City',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'county': 'County',
+        }
+    
+
