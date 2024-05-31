31f23082455b838ad929f1502e8129090016d18c
Alice Dean <elliott.alice.92@gmail.com>
2024-05-31 19:18:29 +0000

    add toasts to bag app

--- bag/views.py
+++ bag/views.py
@@ -26,15 +26,20 @@ def add_to_bag(request, prints_id):
         if prints_id in list(bag.keys()):
             if size in bag[prints_id]['prints_by_size'].keys():
                 bag[prints_id]['prints_by_size'][size] += quantity
+                messages.success(request, f'Updated size {size.upper()} {prints.friendly_name} quantity to {bag[prints_id]["prints_by_size"][size]}.')
             else:
                 bag[prints_id]['prints_by_size'][size] = quantity
+                messages.success(request, f'Added size {size.upper()} {prints.friendly_name} to your bag.')
         else:
             bag[prints_id] = {'prints_by_size': {size: quantity}}
+            messages.success(request, f'Added size {size.upper()} {prints.friendly_name} to your bag')
     else:
         if prints_id in list(bag.keys()):
             bag[prints_id] += quantity
+            messages.success(request, f'Updated {prints.friendly_name} quantity to {bag[prints_id]}')
         else:
             bag[prints_id] = quantity
+            messages.success(request, f'Added {prints.friendly_name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -52,15 +57,19 @@ def adjust_bag(request, prints_id):
     if size:
         if quantity > 0:
             bag[prints_id]['prints_by_size'][size] = quantity
+            messages.success(request, f'Updated size {size.upper()} {prints.friendly_name} quantity to {bag[prints_id]["prints_by_size"][size]}')
         else:
             del bag[prints_id]['prints_by_size'][size]
             if not bag[prints_id]['prints_by_size']:
                 bag.pop(prints_id)
+            messages.success(request, f'Removed size {size.upper()} {prints.friendly_name} from your bag')
     else:
         if quantity > 0:
             bag[prints_id] = quantity
+            messages.success(request, f'Updated {prints.friendly_name} quantity to {bag[prints_id]}')
         else:
             bag.pop(prints_id)
+            messages.success(request, f'Removed {prints.friendly_name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('bag'))
@@ -79,10 +88,13 @@ def remove_from_bag(request, prints_id):
                 del bag[prints_id]['prints_by_size'][size]
                 if not bag[prints_id]['prints_by_size']:
                     bag.pop(prints_id)
+                messages.success(request, f'Removed size {size.upper()} {prints.friendly_name} from your bag')
         else:
             bag.pop(prints_id)
+            messages.success(request, f'Removed {prints.friendly_name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
