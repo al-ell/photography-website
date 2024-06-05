c2180ad574af5c754851910bbf73f3369486750d
Alice Dean <elliott.alice.92@gmail.com>
2024-06-05 18:32:09 +0000

    add order history to profile

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 
 def profile(request):
@@ -17,10 +18,27 @@ def profile(request):
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True, 
+    }
+
+    return render(request, template, context)
+
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, f'Confirmation for past order: {order_number}.')
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
