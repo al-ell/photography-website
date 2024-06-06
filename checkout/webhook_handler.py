c7bf2b925760f7ca5a0ef10e55bcb1247b9a91aa
Alice Dean <elliott.alice.92@gmail.com>
2024-06-06 10:09:48 +0000

    add confirmation emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from shop.models import Prints
-from pofiles.models import UserProfile
+from profiles.models import UserProfile
 
 import stripe
 import json
@@ -14,6 +17,21 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        customer_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )
 
     def handle_event(self, event):
         """
@@ -52,8 +70,8 @@ class StripeWH_Handler:
             profile = UserProfile.object.get(user__username=username)
             if save_info:
                 profile.default_phone_number = shipping_details.phone
-                profile.default_street_address1_name = shipping_details.address.line1
-                profile.default_street_address2_name = shipping_details.address.line2
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
                 profile.default_city_or_town = shipping_details.address.city
                 profile.default_county = shipping_details.address.state
                 profile.default_postcode = shipping_details.address.postal_code
@@ -68,8 +86,8 @@ class StripeWH_Handler:
                     full_name__iexact=shipping_details.name,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
-                    street_address1_name__iexact=shipping_details.address.line1,
-                    street_address2_name__iexact=shipping_details.address.line2,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
                     city_or_town__iexact=shipping_details.address.city,
                     county__iexact=shipping_details.address.state,
                     postcode__iexact=shipping_details.address.postal_code,
@@ -84,6 +102,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | Order already in database',
                 status=200)
@@ -95,8 +114,8 @@ class StripeWH_Handler:
                     user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
-                    street_address1_name=shipping_details.address.line1,
-                    street_address2_name=shipping_details.address.line2,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
                     city_or_town=shipping_details.address.city,
                     county=shipping_details.address.state,
                     postcode=shipping_details.address.postal_code,
@@ -128,6 +147,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
