88715e12c8f0cf980c2235ac6d3af7d067d34bd5
Alice Dean <elliott.alice.92@gmail.com>
2024-06-09 18:50:03 +0000

    add template for contact form emails

--- contact/views.py
+++ contact/views.py
@@ -1,20 +1,44 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .forms import ContactForm
 
 def contact(request):
 
-    if request.method == 'POST':
+    def _send_contact_email(self, form):
+        contact_email = form.email
+        subject = render_to_string(
+            'contact/contact_emails/email_subject.txt',
+            {'form': form,})
+        message = render_to_string(
+        'contact/contact_emails/email_message.txt',
+        { 'form': form, 'reciever': settings.DEFAULT_TO_EMAIL})
+    
+        send_mail(
+            
+            subject,
+            message,
+            contact_email,
+            [settings.DEFAULT_TO_EMAIL]
+        )
 
-        form = ContactForm(request.POST)
-        if form.is_valid():
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST)        
+
+        if form.is_valid(self):            
+            self._send_contact_email(form)
             messages.success(request, "Message sent!")
             return redirect(request, 'home')
+        else:
+            messages.error(request, "Sorry, your form can't be processed at the moment.\
+                            Please check and try again.")
     else:
         form = ContactForm()
-        messages.error(request, "Sorry, your form can't be processed at the moment.\
-                        Please check and try again.")
+
 
     template = 'contact/contact.html'
     context = {
