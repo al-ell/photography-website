4fa8b11567399428afa006730ac8ad81f00fe224
Alice Dean <elliott.alice.92@gmail.com>
2024-06-15 11:45:37 +0000

    responsive images on homepage

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from projects.models import Photo
 
 
 def index(request):
     """ Index view """
+    photo_ids = [47, 8, 70, 31, 1, 31]
+    photos = Photo.objects.filter(id__in=photo_ids)
+
+
+    template = 'home/index.html'
+    context = {
+        'photos': photos,
+    }
     
-    return render(request, 'home/index.html')
+    return render(request, template, context)
