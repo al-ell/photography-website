effdf180b00bc1f3c13b700ed4bb74de75da74e6
Alice Dean <elliott.alice.92@gmail.com>
2024-06-20 10:41:00 +0000

    update AWS - add static

--- gl_photography/settings.py
+++ gl_photography/settings.py
@@ -211,7 +211,6 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https//{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https//{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # payment and stripe
