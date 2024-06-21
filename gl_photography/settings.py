b4211c06e4ea3878470eaff77be98605be6e0bb5
Alice Dean <elliott.alice.92@gmail.com>
2024-06-21 15:25:41 +0000

    update nav collapse

--- gl_photography/settings.py
+++ gl_photography/settings.py
@@ -196,6 +196,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # cache
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 18:00:00 GMT',
+        'CacheControl': 'max-age=94608000'
+    }
     # Bucket configuration
     AWS_STORAGE_BUCKET_NAME = 'gl-photography'
     AWS_S3_REIGON_NAME = 'eu-north-1'
